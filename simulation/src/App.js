f39a567da17a20d97b593904bf8cfc287469a27f
MountainGirl504 <natalia_calt@yahoo.com>
2017-10-13 13:18:46 -0600

    header

--- simulation/src/App.js
+++ simulation/src/App.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  
+  constructor (){
+    super()
+
+    this.state = {
+
+    }
+  }
   
 
 handleClick(){
-
+  
 }
 
   render() {
     return (
       <div>
+        <header><h1>SHELFIE</h1></header>
         <button className='shelfA' onClick={ (e) => this.handleClick(e.target.value)}>Shelf A</button>
         <button className='shelfB'>Shelf B</button>
         <button className='shelfC'>Shelf C</button>
