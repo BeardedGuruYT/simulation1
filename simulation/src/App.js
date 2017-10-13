64987ec734c7904eb00152671e22959052b6728c
MountainGirl504 <natalia_calt@yahoo.com>
2017-10-13 12:37:00 -0600

    frontEnd

--- simulation/src/App.js
+++ simulation/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  
+
+handleClick(){
+
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button className='shelfA' onClick={ (e) => this.handleClick(e.target.value)}>Shelf A</button>
+        <button className='shelfB'>Shelf B</button>
+        <button className='shelfC'>Shelf C</button>
+        <button className='shelfD'>Shelf D</button>
       </div>
+      
     );
   }
 }
